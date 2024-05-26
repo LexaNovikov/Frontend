07bb85b093dbf463cfacf924a0033b9b0725f444
LexaNovikov <lexaproxa228@gmail.com>
2024-05-26 22:28:29 +0300

    опять меняю логин

--- app/store/app-store.js
+++ app/store/app-store.js
@@ -7,7 +7,7 @@ export const useStore = create((set) => ({
     user: null,
     token: null,
     login: (user, token) => {
-        const jwt = getJWT();
+        const token = getJWT();
         set({ isAuth: true, user, token });
         setJWT(token);
     },
